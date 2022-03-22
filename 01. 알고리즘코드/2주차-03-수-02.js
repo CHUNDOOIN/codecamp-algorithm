e5163bde9549693c032eb9488dd2346a3d52a3ab
CHUNDOOIN <chun.doo.in@gmail.com>
2022-03-22 12:21:59 +0900

    알고리즘 22.03.22

---
+++ 01. 알고리즘코드/2주차-03-수-02.js
@@ -0,0 +1,15 @@
+// function solution(s) {
+//   let arr = s.split("");
+//   if (arr.length === 4 || arr.length === 6) {
+//     for (let i = 0; i < arr.length; i++) {
+//       if (isNaN(parseInt(arr[i])) === true) {
+//         return false;
+//       } else {
+//         return true;
+//       }
+//     }
+//   } else {
+//     answer = false;
+//   }
+//   return answer;
+// }
