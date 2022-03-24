14b593d4bd738ea716bf4ae8c2a027085d9dd9cc
CHUNDOOIN <chun.doo.in@gmail.com>
2022-03-24 09:43:56 +0900

    22.03.24 알고리즘

--- 01. 알고리즘코드/2주차-05-금-02.js
+++ 01. 알고리즘코드/2주차-05-금-02.js
@@ -1,3 +1,11 @@
-function solution(s) {
-  return s.split("").sort().reverse().join("");
+function solution(array, commands) {
+  var answer = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    let sort = array
+      .slice(commands[i][0] - 1, commands[i][1])
+      .sort((a, b) => a - b);
+    answer.push(sort[commands[i][3 - 1] - 1]);
+  }
+  return answer;
 }
