54a686119c6548965e6e5cd6e8756d17ea8eb42d
CHUNDOOIN <chun.doo.in@gmail.com>
2022-04-11 18:16:58 +0900

    22.04.11 알고리즘

--- 01. 알고리즘코드/5주차-01-월-01.js
+++ 01. 알고리즘코드/5주차-01-월-01.js
@@ -16,5 +16,12 @@ function solution(participant, completion) {
 }
 
 // solution (["leo", "kiki", "eden"], ["eden", "kiki"]	)
-
 // solution (["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]	)
+
+// splice 메서드!!!
+// 1. 배열 메서드
+// 2. 원하는 위치(인덱스)의 데이터를 제거하거나 추가할 수 있다.
+// 3. 원본이 저장!
+
+// arr.sort((a, b) => a > b ? 1 : -1) 오름차순
+// arr.sort((a, b) => a < b ? 1 : -1) 내림차순
