631dcac738eeeeafe6f032ffde6068eaa2fa0c72
CHUNDOOIN <chun.doo.in@gmail.com>
2022-04-12 18:21:59 +0900

    22.04.12 알고리즘

---
+++ 01. 알고리즘코드/5주차-02-화-01.js
@@ -0,0 +1,37 @@
+function solution(answers) {
+  let one = [1, 2, 3, 4, 5];
+  let two = [2, 1, 2, 3, 2, 4, 2, 5];
+  let three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  let count = [0, 0, 0];
+
+  let answer = [];
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === one[i % one.length]) {
+      count[0]++;
+    }
+  }
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === two[i % two.length]) {
+      count[1]++;
+    }
+  }
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === three[i % three.length]) {
+      count[2]++;
+    }
+  }
+
+  let max = Math.max(...count);
+
+  for (let i = 0; i < count.length; i++) {
+    if (max === count[i]) {
+      answer.push(i + 1);
+    }
+  }
+
+  return answer;
+}
